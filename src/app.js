59700bb3ec86fef5098b8a33a984218dd3b5ffb9
DanielCorreaV <correavegadaniel@gmail.com>
2026-05-20 03:28:25 -0500

    Update short URL format to include '/r/' prefix for copied links

--- src/app.js
+++ src/app.js
@@ -202,7 +202,7 @@ async function selectAsset(code) {
 
 function copyCurrentShortUrl() {
   if (!selectedCode) return;
-  const targetUrl = `${API_BASE_URL}/${selectedCode}`;
+  const targetUrl = `${API_BASE_URL}/r/${selectedCode}`;
   navigator.clipboard
     .writeText(targetUrl)
     .then(() => alert("¡Enlace corto copiado al portapapeles con éxito!"));
